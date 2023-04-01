029f134a9170e46c84e0d41d5dd3a3f6755f11b4
Marlonperez999 <69155466+Marlonperez999@users.noreply.github.com>
2023-04-01 16:05:27 -0500

    tarea clase 2

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,21 +1,23 @@
 import pygame
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 
 class Dinosour(Sprite):
     X_POS = 80
     Y_POS = 310
     JUMP_SPEED = 8.5
+    Y_POS_DUCK = 340
 
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
-        self.dino_run = True
         self.step_index = 0
+        self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_speed = self.JUMP_SPEED
 
     def update(self, user_input):
@@ -23,15 +25,22 @@ class Dinosour(Sprite):
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()    
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-        elif not self.dino_jump:
+            self.dino_duck = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
             self.dino_jump = False
+        elif not self.dino_jump:
+            # self.dino_jump = False
             self.dino_run = True
 
-        if self.step_index > 10:
+        if self.step_index >= 10:
             self.step_index = 0
 
     def draw(self, screen):
@@ -55,4 +64,8 @@ class Dinosour(Sprite):
             self.jump_speed = self.JUMP_SPEED
 
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
