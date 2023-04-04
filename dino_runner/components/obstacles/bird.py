5b90483010ff4c39b2c2e02245c83c2ad6ea2cd0
Marlonperez999 <69155466+Marlonperez999@users.noreply.github.com>
2023-04-04 15:41:39 -0500

    class three homework update

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -4,7 +4,16 @@ from dino_runner.components.obstacles.obstacle import Obstacle
 
 class Bird(Obstacle):
     def __init__(self, image):
-        self.type = random.randint(0,1)
-        random_height = random.randint(220,305)
+        self.type = random.randint(0,1)        
         super().__init__(image,self.type,'bird')
-        self.rect.y = random_height
+        self.rect.y = 0
+        self.bird_height()
+
+    def bird_height(self):
+        random_height = random.randint(0,2)
+        if random_height == 0:
+            self.rect.y = 305
+        elif random_height == 1:
+            self.rect.y = 240
+        else:
+            self.rect.y=220
