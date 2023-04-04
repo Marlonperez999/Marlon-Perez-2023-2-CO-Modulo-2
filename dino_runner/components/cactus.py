a371d25ed3f43b467b001a9a67edfc034bf7913f
Marlonperez999 <69155466+Marlonperez999@users.noreply.github.com>
2023-04-03 23:43:42 -0500

    updated class 3

--- dino_runner/components/cactus.py
+++
@@ -1,9 +0,0 @@
-
-from dino_runner.components.obstacule import Obstacule
-
-class Cactus(Obstacule):
-    
-    def __init__(self,image):
-        self.type = random.randint(0,2)
-        super().__init__(image, self.type)
-        self.rect.y = 325
